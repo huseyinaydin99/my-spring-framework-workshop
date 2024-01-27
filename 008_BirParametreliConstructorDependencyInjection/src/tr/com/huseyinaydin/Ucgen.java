fb55db1f3fd1628037d004e033f0d2aecf736caa
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-01-27 10:12:43 +0300

    008_BirParametreliConstructorDependencyInjection projesi yazıldı.
    008_BirParametreliConstructorDependencyInjection projesi yazıldı.

---
+++ 008_BirParametreliConstructorDependencyInjection/src/tr/com/huseyinaydin/Ucgen.java
@@ -0,0 +1,32 @@
+package tr.com.huseyinaydin;
+
+//بسم الله الرحمن الرحيم
+
+/**
+* 
+* @author Huseyin_Aydin
+* @since 1994
+* @category Java, Spring Framework.
+* 
+*/
+
+public class Ucgen {
+	
+	private String cesit;
+
+	public Ucgen(String aaa) {
+		this.cesit = aaa;
+	}
+
+	public void setCesit(String cesit) {
+		this.cesit = cesit;
+	}
+
+	public String getCesit() {
+		return cesit;
+	}
+
+	public void ciz() {
+		System.out.println(getCesit() + " Üçgen çiz.");
+	}
+}
