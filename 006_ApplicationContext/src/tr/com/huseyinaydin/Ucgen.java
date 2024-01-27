43575db5008d99ac706ae41ecdf9f694e960e89c
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-01-27 10:02:50 +0300

    006_ApplicationContext proje yazıldı.
    006_ApplicationContext proje yazıldı.

---
+++ 006_ApplicationContext/src/tr/com/huseyinaydin/Ucgen.java
@@ -0,0 +1,18 @@
+package tr.com.huseyinaydin;
+
+//بسم الله الرحمن الرحيم
+
+/**
+* 
+* @author Huseyin_Aydin
+* @since 1994
+* @category Java, Spring Framework.
+* 
+*/
+
+public class Ucgen {
+
+	public void ciz() {
+		System.out.println("Üçgen çiz.");
+	}
+}
