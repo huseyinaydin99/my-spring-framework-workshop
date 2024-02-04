c88d5f146ba50cf333a66819e91ee98fb2ab2e94
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-02-04 12:53:03 +0300

    Spring AspectJ kütüphanesi ile örnekler yazıldı.

---
+++ 071_AOP-AspectJ-@AfterThrowing/src/main/java/tr/com/huseyinaydin/aop/MetotCagiriIstisnaSonrasiYapilacaklar.java
@@ -0,0 +1,28 @@
+package tr.com.huseyinaydin.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+
+//بسم الله الرحمن الرحيم
+
+/**
+* 
+* @author Huseyin_Aydin
+* @since 1994
+* @category Java, Spring Framework.
+* 
+*/
+
+@Aspect
+public class MetotCagiriIstisnaSonrasiYapilacaklar {
+
+	@AfterThrowing(pointcut = "execution(* "
+			+ "tr.com.huseyinaydin.IPersonelServis.adiSoyadiKontrolEt(..))", throwing = "hata")
+	public void istisnadanSonrakiGorev(JoinPoint joinPoint, Throwable hata) {
+		System.out.println("\n--- AspectJ --- " + joinPoint.getSignature().getName() + " metodun"
+				+ " bir İSTİSNADAN SONRA" + " çalışan metot.");
+		System.out
+				.println(joinPoint.getSignature().getName() + " metodunda meydana gelen istisna türü : " + hata + "\n");
+	}
+}
---
+++ 072_AOP-AspectJ-@Around/src/main/java/tr/com/huseyinaydin/IPersonelServis.java
@@ -0,0 +1,18 @@
+package tr.com.huseyinaydin;
+
+//بسم الله الرحمن الرحيم
+
+/**
+* 
+* @author Huseyin_Aydin
+* @since 1994
+* @category Java, Spring Framework.
+* 
+*/
+
+public interface IPersonelServis {			
+	public void adiSoyadiKontrolEt() ;
+//	public void selamVer(String mesaj);
+	public String selamVer(String mesaj);				
+	public void personelBilgisiniVer();
+}
